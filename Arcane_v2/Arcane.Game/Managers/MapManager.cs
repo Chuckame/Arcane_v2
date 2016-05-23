f38b892cfc522517e71d332a6a1220a07d62bd49
AntoineM <toinou_17@hotmail.fr>
2016-05-23 22:39:41 +0200

    starting  maps manager

---
+++ Arcane_v2/Arcane.Game/Managers/MapManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcane.Game.Managers
+{
+    public class MapManager
+    {
+        #region singleton
+        private static MapManager _instance = new MapManager();
+        public static MapManager Instance { get { return _instance; } }
+        #endregion
+
+        public
+
+        private MapManager()
+        {
+
+        }
+    }
+}
